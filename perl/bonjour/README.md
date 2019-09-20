2f5371c523f06cb541b74e0fefde055603bdc0d3
ssinn <spencer.sinn@gmail.com>
2019-09-20 15:49:39 -0500

    Created README.md and removed some commented-out bits from bonjour-listen.pl

---
+++ perl/bonjour/README.md
@@ -0,0 +1,16 @@
+bonjour-connect.pl
+#----------------#
+Attempts to make a basic socket connection using mDNS.
+If the socket connection is succesful, write a simple
+message to the socket.
+
+bonjour-listen.pl
+#---------------#
+Listens for all bonjour traffic on the network segment
+
+mdns.pl
+#-----#
+
+README.md
+#-------#
+This cruft.
--- perl/bonjour/bonjour-listen.pl
+++ perl/bonjour/bonjour-listen.pl
@@ -7,12 +7,10 @@ use strict;
 use warnings;
 use Net::Bonjour;
 
-#while (1) {
-	foreach my $res ( Net::Bonjour->all_services ) {
-		printf "-- %s (%s) ---\n", $res->service, $res->protocol;
-		$res->discover;
-		foreach my $entry ( $res->entries ) {
-			printf "\t%s (%s:%s)\n", $entry->name, $entry->address, $entry->port;
-		}
+foreach my $res ( Net::Bonjour->all_services ) {
+	printf "-- %s (%s) ---\n", $res->service, $res->protocol;
+	$res->discover;
+	foreach my $entry ( $res->entries ) {
+		printf "\t%s (%s:%s)\n", $entry->name, $entry->address, $entry->port;
 	}
-#}
+}
